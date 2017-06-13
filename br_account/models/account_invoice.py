8f6a0fecd8817d8da073e7d52735391df7f818fc
Michell Stuttgart <michellstut@gmail.com>
2017-06-13 15:16:28 -0300

    [IMP] Campos 'financial_operation_id' e 'title_type_id' movidos para wizard durante a criacao das parcelas

--- br_account/models/account_invoice.py
+++ br_account/models/account_invoice.py
@@ -116,17 +116,6 @@ class AccountInvoice(models.Model):
                                  states={'draft': [('readonly', False)]},
                                  string='Parcelas')
 
-    financial_operation_id = fields.Many2one('account.financial.operation',
-                                             states={'draft': [
-                                                 ('readonly', False)]},
-                                             readonly=True,
-                                             string=u'Operação Financeira')
-
-    title_type_id = fields.Many2one('account.title.type',
-                                    readonly=True,
-                                    states={'draft': [('readonly', False)]},
-                                    string=u'Tipo de Título')
-
     receivable_move_line_ids = fields.Many2many(
         'account.move.line', string='Receivable Move Lines',
         compute='_compute_receivables')
@@ -468,70 +457,22 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def action_create_periodic_entry(self):
-        """Metodos responsaveis por criar parcelas a partir"""
+        """Abre wizard para gerar pagamentos periodicos"""
+        self.ensure_one()
+        action = {
+            'type': 'ir.actions.act_window',
+            'res_model': 'br_account.invoice.parcel.wizard',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'context': {
+                'default_payment_term_id': self.payment_term_id.id,
+                'default_date_invoice': self.date_invoice,
+            },
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
 
-        for inv in self:
-
-            ctx = dict(self._context, lang=inv.partner_id.lang)
-
-            if not inv.date_invoice:
-                inv.with_context(ctx).write(
-                    {'date_invoice': fields.Date.context_today(self)})
-
-            date_invoice = inv.date_invoice
-            company_currency = inv.company_id.currency_id
-
-            # create move lines (one per invoice line + eventual taxes and
-            # analytic lines)
-            iml = inv.invoice_line_move_line_get()
-            iml += inv.tax_line_move_line_get()
-
-            diff_currency = inv.currency_id != company_currency
-
-            total, total_currency, iml = inv.with_context(
-                ctx).compute_invoice_totals(company_currency, iml)
-
-            if inv.payment_term_id:
-                lines = inv.with_context(ctx).payment_term_id.with_context(
-                    currency_id=company_currency.id).compute(total,
-                                                             date_invoice)[0]
-
-                res_amount_currency = total_currency
-                ctx['date'] = date_invoice
-
-                # Removemos as parcelas adicionadas anteriormente
-                inv.parcel_ids.unlink()
-
-                for i, t in enumerate(lines):
-                    if inv.currency_id != company_currency:
-                        amount_currency = \
-                            company_currency.with_context(ctx).compute(
-                                t[1], inv.currency_id)
-                    else:
-                        amount_currency = False
-
-                    # last line: add the diff
-                    res_amount_currency -= amount_currency or 0
-
-                    if i + 1 == len(lines):
-                        amount_currency += res_amount_currency
-
-                    values = {
-                        'name': str(i + 1).zfill(2),
-                        'parceling_value': t[1],
-                        'date_maturity': t[0],
-                        'financial_operation_id':
-                            inv.financial_operation_id.id,
-                        'title_type_id': inv.title_type_id.id,
-                        'company_currency_id': (diff_currency and
-                                                inv.currency_id.id),
-                        'invoice_id': inv.id,
-                    }
-
-                    obj = self.env['br_account.invoice.parcel'].create(values)
-                    # Chamamos o onchange para que a quantidade de dias seja
-                    # calculado
-                    obj._onchange_date_maturity()
+        return action
 
     @api.multi
     def action_invoice_cancel_paid(self):
