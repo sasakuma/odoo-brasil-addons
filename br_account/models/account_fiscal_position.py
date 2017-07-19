aa679682585fd9dcdadfc4f958d037bae4201634
Michell Stuttgart <michellstut@gmail.com>
2017-07-19 10:03:28 -0300

    [IMP] Adicinado @api.model no metodo '_default_company_id'

--- br_account/models/account_fiscal_position.py
+++ br_account/models/account_fiscal_position.py
@@ -80,6 +80,7 @@ class AccountFiscalPositionTaxRule(models.Model):
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
+    @api.model
     def _default_company_id(self):
         return self.env.user.company_id
 
