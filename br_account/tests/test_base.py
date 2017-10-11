dbe35e27bda1a45eb02569ebae41c9207fec936f
Michell Stuttgart <michellstut@gmail.com>
2017-10-11 15:43:25 -0300

    [REF] Formatacao de codigo

--- br_account/tests/test_base.py
+++ br_account/tests/test_base.py
@@ -16,16 +16,19 @@ class TestBaseBr(TransactionCase):
             'code': '0201.20.20',
             'name': 'Furniture'
         })
+
         self.default_product = self.env['product.product'].create({
             'name': 'Normal Product',
             'fiscal_classification_id': self.default_ncm.id,
             'list_price': 15.0
         })
+
         self.st_product = self.env['product.product'].create({
             'name': 'Product for ICMS ST',
             'fiscal_classification_id': self.default_ncm.id,
             'list_price': 25.0
         })
+
         self.service = self.env['product.product'].create({
             'name': 'Normal Service',
             'default_code': '25',
@@ -33,6 +36,7 @@ class TestBaseBr(TransactionCase):
             'fiscal_type': 'service',
             'list_price': 50.0
         })
+
         self.revenue_account = self.env['account.account'].create({
             'code': '3.0.0',
             'name': 'Receita de Vendas',
@@ -40,6 +44,7 @@ class TestBaseBr(TransactionCase):
                 'account.data_account_type_revenue').id,
             'company_id': self.main_company.id
         })
+
         self.receivable_account = self.env['account.account'].create({
             'code': '1.0.0',
             'name': 'Conta de Recebiveis',
@@ -50,6 +55,7 @@ class TestBaseBr(TransactionCase):
         })
 
         self.tax_model = self.env['account.tax']
+
         self.pis_500 = self.tax_model.create({
             'name': "PIS",
             'amount_type': 'division',
@@ -58,6 +64,7 @@ class TestBaseBr(TransactionCase):
             'sequence': 1,
             'price_include': True,
         })
+
         self.cofins_1500 = self.tax_model.create({
             'name': "Cofins",
             'amount_type': 'division',
@@ -66,6 +73,7 @@ class TestBaseBr(TransactionCase):
             'sequence': 2,
             'price_include': True,
         })
+
         self.ipi_700 = self.tax_model.create({
             'name': "IPI",
             'amount_type': 'percent',
@@ -73,6 +81,7 @@ class TestBaseBr(TransactionCase):
             'amount': 7,
             'sequence': 3,
         })
+
         self.icms_1700 = self.tax_model.create({
             'name': "ICMS",
             'amount_type': 'division',
@@ -81,6 +90,7 @@ class TestBaseBr(TransactionCase):
             'sequence': 4,
             'price_include': True,
         })
+
         self.icms_inter_1200 = self.tax_model.create({
             'name': "ICMS Inter",
             'amount_type': 'division',
@@ -89,6 +99,7 @@ class TestBaseBr(TransactionCase):
             'sequence': 4,
             'price_include': True,
         })
+
         self.icms_st_1800 = self.tax_model.create({
             'name': "ICMS ST",
             'amount_type': 'icmsst',
@@ -96,6 +107,7 @@ class TestBaseBr(TransactionCase):
             'amount': 18,
             'price_include': False,
         })
+
         self.icms_difal_inter_700 = self.tax_model.create({
             'name': "ICMS Difal Inter",
             'amount_type': 'division',
@@ -103,6 +115,7 @@ class TestBaseBr(TransactionCase):
             'amount': 7,
             'price_include': True,
         })
+
         self.icms_difal_intra_1700 = self.tax_model.create({
             'name': "ICMS Difal Intra",
             'amount_type': 'division',
@@ -110,6 +123,7 @@ class TestBaseBr(TransactionCase):
             'amount': 17,
             'price_include': True,
         })
+
         self.icms_fcp_200 = self.tax_model.create({
             'name': "FCP",
             'amount_type': 'division',
@@ -117,6 +131,7 @@ class TestBaseBr(TransactionCase):
             'amount': 2,
             'price_include': True,
         })
+
         self.issqn_500 = self.tax_model.create({
             'name': "ISSQN",
             'amount_type': 'division',
@@ -124,6 +139,7 @@ class TestBaseBr(TransactionCase):
             'amount': 5,
             'price_include': True,
         })
+
         self.ii_6000 = self.tax_model.create({
             'name': "II",
             'amount_type': 'division',
