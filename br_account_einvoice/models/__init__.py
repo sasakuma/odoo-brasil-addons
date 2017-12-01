528653c105f2984da2620024085aad9319df9ca5
Michell Stuttgart <michellstut@gmail.com>
2017-12-01 15:04:11 -0200

    [DEL] Model de 'account.config.setting' removido do modulo 'br_account_einvoice'

--- br_account_einvoice/models/__init__.py
+++ br_account_einvoice/models/__init__.py
@@ -2,10 +2,7 @@
 # © 2016 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
-from . import account_config_settings
 from . import account_invoice
 from . import invoice_electronic
 from . import invoice_electronic_event
 from . import invoice_electronic_item
-from . import res_company
