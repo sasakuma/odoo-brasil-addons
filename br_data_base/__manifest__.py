edc91d07537967b7b0af47ed17d9f4b991f41212
Michell Stuttgart <michellstut@gmail.com>
2018-02-20 17:38:16 -0300

    [MIG] br_data_base: Migration to 11.0

--- br_data_base/__manifest__.py
+++ br_data_base/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2009  Renato Lima - Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,7 +6,7 @@
     'description': 'Brazilian Localisation Data Extension for Base',
     'license': 'AGPL-3',
     'author': 'Akretion, OpenERP Brasil',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'depends': [
         'br_base',
     ],
@@ -17,8 +16,7 @@
         'data/res.country.state.csv',
         'data/br_base_data.xml',
     ],
-    'demo': [],
     'category': 'Localisation',
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
