2d1d3a62bb88f85b4202577db72bf40968c319f0
Michell Stuttgart <michellstut@gmail.com>
2018-02-27 11:54:40 -0300

    [MIG] br_coa: Migration to 11.0

--- br_coa/__manifest__.py
+++ br_coa/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,24 +6,24 @@
     'name': 'Plano de Contas Simplificado Brasil',
     'summary': """Plano de contas simplificado""",
     'description': """Plano de contas simplificado""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Localization',
     'author': 'Trustcode',
     'license': 'AGPL-3',
     'website': 'http://www.trustcode.com.br',
     'contributors': [
         'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Michell Stuttgart <michellstut@gmail.com>',
     ],
     'depends': [
-        'account', 'br_account'
+        'account',
+        'br_account'
     ],
     'data': [
         'data/br_chart_data.xml',
         'data/account.account.template.csv',
         'data/account_tax_template_data.xml',
-        # TODO Achar uma forma de carregar o template
-        # 'data/account_chart_template_data.yml',
     ],
     'active': False,
-    'installable': False,
+    'installable': True,
 }
