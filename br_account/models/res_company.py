398eee2e2bde1f5579fdec0652859ede78a042c7
Aldo_Soares <soares_aldo@hotmail.com>
2018-04-19 17:39:10 -0300

    [REF] Transfere validação antes feita em módulo br_boleto para br_account

--- br_account/models/res_company.py
+++ br_account/models/res_company.py
@@ -3,8 +3,9 @@
 # © 2017 Michell Stuttgart, MultidadosTI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
+from odoo.tools.translate import _
 
 COMPANY_FISCAL_TYPE = [
     ('1', 'Simples Nacional'),
@@ -39,3 +40,31 @@ class ResCompany(models.Model):
                                           string='CNAE Secundários')
 
     accountant_id = fields.Many2one('res.partner', string="Contador")
+
+    @api.multi
+    def validate(self):
+        error = ''
+        if not self.partner_id.legal_name:
+            error += _('-Legal Name\n')
+        if not self.cnpj_cpf:
+            error += _('-CNPJ/CPF \n')
+        if not self.district:
+            error += _('-District\n')
+        if not self.zip:
+            error += _('-ZIP\n')
+        if not self.city_id:
+            error += _('-City\n')
+        if not self.country_id:
+            error += _('-Country\n')
+        if not self.street:
+            error += _('-Street\n')
+        if not self.number:
+            error += _('-Number\n')
+        if not self.state_id:
+            error += _('-State\n')
+
+        message = _('Company: %s\nMissing Fields:\n%s\n\n')
+        if error:
+            return message % (self.name, error)
+        else:
+            return ''
--- br_account/models/res_partner.py
+++ br_account/models/res_partner.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
-
+from odoo.tools.translate import _
 
 class ResPartner(models.Model):
     _name = 'res.partner'
@@ -62,3 +62,31 @@ class ResPartner(models.Model):
                 if price['partner_id'] in child_ids:
                     total += price['total']
             partner.total_invoiced = total
+
+    @api.multi
+    def validate(self):
+        error = ''
+        if self.is_company and not self.legal_name:
+            error += _('-Legal Name\n')
+        if not self.cnpj_cpf:
+            error += _('-CNPJ/CPF \n')
+        if not self.district:
+            error += _('-District\n')
+        if not self.zip:
+            error += _('-ZIP\n')
+        if not self.city_id:
+            error += _('-City\n')
+        if not self.country_id:
+            error += _('-Country\n')
+        if not self.street:
+            error += _('-Street\n')
+        if not self.number:
+            error += _('-Number\n')
+        if not self.state_id:
+            error += _('-State\n')
+
+        message = _('Client: %s\nMissing Fields:\n%s\n\n')
+        if error:
+            return message % (self.name, error)
+        else:
+            return ''
