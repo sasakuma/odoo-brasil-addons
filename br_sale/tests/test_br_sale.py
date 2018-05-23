2905a5dc2a489d878e3ae6a204fc8f48a2ec5aa3
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-05-23 10:19:53 -0300

    [IMP] Adiciona teste aos modulos de geração de parcelas nas cotações

--- br_sale/tests/test_br_sale.py
+++ br_sale/tests/test_br_sale.py
@@ -1,12 +1,13 @@
 # © 2016 Danimar Ribeiro, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests.common import TransactionCase
 
 
-class TestSaleOrder(TransactionCase):
+class TestBrSaleOrder(TransactionCase):
     def setUp(self):
-        super(TestSaleOrder, self).setUp()
+        super(TestBrSaleOrder, self).setUp()
         self.main_company = self.env.ref('base.main_company')
         self.currency_real = self.env.ref('base.BRL')
         self.revenue_account = self.env['account.account'].create({
@@ -223,7 +224,9 @@ class TestSaleOrder(TransactionCase):
 
         default_saleorder = {
             'fiscal_position_id': self.fpos.id,
-            'order_line': order_line_data
+            'order_line': order_line_data,
+            'payment_term_id': self.env.ref('account.account_payment_term').id,
+            'quotation_date': '2017-07-01',
         }
 
         self.sales_order = self.env['sale.order'].create({
@@ -237,6 +240,13 @@ class TestSaleOrder(TransactionCase):
             'name': 'SO 002',
             'partner_id': self.partner_juridica.id
         })
+
+        self.title_type = self.env.ref('br_account.account_title_type_2')
+        self.financial_operation = self.env.ref(
+            'br_account.account_financial_operation_6')
+
+        self.sales_order[0].generate_parcel_entry(self.financial_operation,
+                                                  self.title_type)
 
     def test_sale_order_to_invoice(self):
         for item in self.sales_order:
@@ -274,3 +284,48 @@ class TestSaleOrder(TransactionCase):
                 self.assertEqual(line.ipi_reducao_bc, inv_line.ipi_reducao_bc)
                 self.assertEqual(line.pis_cst, inv_line.pis_cst)
                 self.assertEqual(line.cofins_cst, inv_line.cofins_cst)
+
+    def test_generate_parcel_entry(self):
+
+        for inv in self.sales_order:
+            for parcel in inv.parcel_ids:
+                self.assertEqual(parcel.date_maturity, '2017-08-31')
+                self.assertEqual(parcel.old_date_maturity, '2017-08-31')
+                self.assertEqual(parcel.name, '01')
+                self.assertEqual(parcel.parceling_value, inv.amount_total)
+                self.assertEqual(parcel.financial_operation_id.id,
+                                 self.financial_operation.id)
+                self.assertEqual(parcel.title_type_id.id, self.title_type.id)
+
+    def test_action_open_periodic_entry_wizard(self):
+
+            # O metodo deve receber apenas um record, caso contrário o retorno
+            # sera comprometido. Aqui, verificamos se o metodo dispara excecao
+            # quando o mesmo e invocado com mais de um record
+        with self.assertRaises(ValueError):
+            self.sales_order.action_open_periodic_entry_wizard()
+
+        for inv in self.sales_order:
+            action = inv.action_open_periodic_entry_wizard()
+
+            # Verificamos se as chaves estao no dicionario.
+            # Isso e feita a fim de detectar se alguma chave foi removida
+            self.assertIn('type', action)
+            self.assertIn('res_model', action)
+            self.assertIn('view_type', action)
+            self.assertIn('view_mode', action)
+            self.assertIn('views', action)
+            self.assertIn('target', action)
+            self.assertIn('context', action)
+            self.assertIn('default_payment_term_id', action['context'])
+
+            # Verificamos o valor das chaves
+            self.assertEqual(action['type'], 'ir.actions.act_window')
+            self.assertEqual(action['res_model'], 'br_sale.parcel.wizard')
+            self.assertEqual(action['view_type'], 'form')
+            self.assertEqual(action['view_mode'], 'form')
+            self.assertEqual(action['views'], [(False, 'form')])
+            self.assertEqual(action['target'], 'new')
+
+            self.assertEqual(action['context']['default_payment_term_id'],
+                             inv.payment_term_id.id)
