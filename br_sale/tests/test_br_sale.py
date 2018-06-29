19236569f5b0f8463bde58733327428e32aed6d3
Michell Stuttgart <michellstut@gmail.com>
2018-06-29 17:58:53 -0300

    [IMP] Atualiza teste de acordo com nova funcionalidade de preencher impostos

--- br_sale/tests/test_br_sale.py
+++ br_sale/tests/test_br_sale.py
@@ -6,6 +6,7 @@ from odoo.tests.common import TransactionCase
 
 
 class TestBrSaleOrder(TransactionCase):
+
     def setUp(self):
         super(TestBrSaleOrder, self).setUp()
         self.main_company = self.env.ref('base.main_company')
@@ -94,7 +95,8 @@ class TestBrSaleOrder(TransactionCase):
         })
 
         self.tax_model = self.env['account.tax']
-        self.pis = self.tax_model.create({
+
+        self.pis_500 = self.tax_model.create({
             'name': "PIS",
             'amount_type': 'division',
             'domain': 'pis',
@@ -103,7 +105,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.cofins = self.tax_model.create({
+        self.cofins_1500 = self.tax_model.create({
             'name': "Cofins",
             'amount_type': 'division',
             'domain': 'cofins',
@@ -112,7 +114,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.ipi = self.tax_model.create({
+        self.ipi_700 = self.tax_model.create({
             'name': "IPI",
             'amount_type': 'percent',
             'domain': 'ipi',
@@ -120,7 +122,7 @@ class TestBrSaleOrder(TransactionCase):
             'sequence': 3,
         })
 
-        self.icms = self.tax_model.create({
+        self.icms_1700 = self.tax_model.create({
             'name': "ICMS",
             'amount_type': 'division',
             'domain': 'icms',
@@ -129,7 +131,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.icms_inter = self.tax_model.create({
+        self.icms_inter_1200 = self.tax_model.create({
             'name': "ICMS Inter",
             'amount_type': 'division',
             'domain': 'icms',
@@ -138,7 +140,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.icms_st = self.tax_model.create({
+        self.icms_st_1800 = self.tax_model.create({
             'name': "ICMS ST",
             'amount_type': 'icmsst',
             'domain': 'icmsst',
@@ -146,7 +148,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': False,
         })
 
-        self.icms_difal_inter = self.tax_model.create({
+        self.icms_difal_inter_700 = self.tax_model.create({
             'name': "ICMS Difal Inter",
             'amount_type': 'division',
             'domain': 'icms_inter',
@@ -154,7 +156,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.icms_difal_intra = self.tax_model.create({
+        self.icms_difal_intra_1700 = self.tax_model.create({
             'name': "ICMS Difal Intra",
             'amount_type': 'division',
             'domain': 'icms_intra',
@@ -162,7 +164,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.icms_fcp = self.tax_model.create({
+        self.icms_fcp_200 = self.tax_model.create({
             'name': "FCP",
             'amount_type': 'division',
             'domain': 'fcp',
@@ -170,7 +172,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.issqn = self.tax_model.create({
+        self.issqn_500 = self.tax_model.create({
             'name': "ISSQN",
             'amount_type': 'division',
             'domain': 'issqn',
@@ -178,7 +180,7 @@ class TestBrSaleOrder(TransactionCase):
             'price_include': True,
         })
 
-        self.ii = self.tax_model.create({
+        self.ii_6000 = self.tax_model.create({
             'name': "II",
             'amount_type': 'division',
             'domain': 'ii',
@@ -197,7 +199,7 @@ class TestBrSaleOrder(TransactionCase):
                  'product_uom': self.default_product.uom_id.id,
                  'product_uom_qty': 10.0,
                  'name': 'product test 5',
-                 'price_unit': 100.00,
+                 'price_unit': self.default_product.list_price,
                  'cfop_id': self.env.ref(
                      'br_data_account_product.cfop_5101').id,
                  'icms_cst_normal': '40',
@@ -213,7 +215,7 @@ class TestBrSaleOrder(TransactionCase):
                  'product_uom': self.service.uom_id.id,
                  'product_uom_qty': 10.0,
                  'name': 'product test 5',
-                 'price_unit': 100.00,
+                 'price_unit': self.service.list_price,
                  'cfop_id': self.env.ref(
                      'br_data_account_product.cfop_5101').id,
                  'pis_cst': '01',
@@ -260,30 +262,57 @@ class TestBrSaleOrder(TransactionCase):
 
                 inv_line = line.invoice_lines[0]
 
-                self.assertEqual(line.icms_cst_normal,
-                                 inv_line.icms_cst_normal)
-                self.assertEqual(line.icms_csosn_simples,
-                                 inv_line.icms_csosn_simples)
+                self.assertEqual(line.valor_desconto, inv_line.valor_desconto)
+                self.assertEqual(line.icms_cst_normal, inv_line.icms_cst_normal)
+                self.assertEqual(line.icms_csosn_simples, inv_line.icms_csosn_simples)
+                self.assertEqual(line.tax_icms_id, inv_line.tax_icms_id)
+                self.assertEqual(line.tax_icms_st_id, inv_line.tax_icms_st_id)
+                self.assertEqual(line.tax_icms_inter_id, inv_line.tax_icms_inter_id)
+                self.assertEqual(line.tax_icms_intra_id, inv_line.tax_icms_intra_id)
+                self.assertEqual(line.tax_icms_fcp_id, inv_line.tax_icms_fcp_id)
+                self.assertEqual(line.tax_simples_id, inv_line.tax_simples_id)
+                self.assertEqual(line.tax_ipi_id, inv_line.tax_ipi_id)
+                self.assertEqual(line.tax_pis_id, inv_line.tax_pis_id)
+                self.assertEqual(line.tax_cofins_id, inv_line.tax_cofins_id)
+                self.assertEqual(line.tax_ii_id, inv_line.tax_ii_id)
+                self.assertEqual(line.tax_issqn_id, inv_line.tax_issqn_id)
+                self.assertEqual(line.tax_csll_id, inv_line.tax_csll_id)
+                self.assertEqual(line.tax_irrf_id, inv_line.tax_irrf_id)
+                self.assertEqual(line.tax_inss_id, inv_line.tax_inss_id)
+                self.assertEqual(line.product_id.fiscal_type, inv_line.product_type)
+                self.assertEqual(line.company_id.fiscal_type, inv_line.company_fiscal_type)
                 self.assertEqual(line.cfop_id, inv_line.cfop_id)
-                self.assertEqual(line.product_id.fiscal_classification_id,
-                                 inv_line.fiscal_classification_id)
-                self.assertEqual(line.fiscal_position_id.service_type_id,
-                                 inv_line.service_type_id)
+                self.assertEqual(line.product_id.fiscal_classification_id, inv_line.fiscal_classification_id)
+                self.assertEqual(line.fiscal_position_id.service_type_id, inv_line.service_type_id)
                 self.assertEqual(line.product_id.origin, inv_line.icms_origem)
-
-                self.assertEqual(line.incluir_ipi_base,
-                                 inv_line.incluir_ipi_base)
-                self.assertEqual(line.icms_st_aliquota_mva,
-                                 inv_line.icms_st_aliquota_mva)
-                self.assertEqual(line.icms_aliquota_reducao_base,
-                                 inv_line.icms_aliquota_reducao_base)
-                self.assertEqual(line.icms_st_aliquota_reducao_base,
-                                 inv_line.icms_st_aliquota_reducao_base)
+                self.assertEqual(line.incluir_ipi_base, inv_line.incluir_ipi_base)
+                self.assertEqual(line.icms_st_aliquota_mva, inv_line.icms_st_aliquota_mva)
+                self.assertEqual(line.icms_aliquota_inter_part, inv_line.icms_aliquota_inter_part)
+                self.assertEqual(line.icms_aliquota_reducao_base, inv_line.icms_aliquota_reducao_base)
+                self.assertEqual(line.icms_st_aliquota_reducao_base, inv_line.icms_st_aliquota_reducao_base)
+                self.assertEqual(line.icms_st_aliquota_deducao, inv_line.icms_st_aliquota_deducao)
+                self.assertEqual(line.icms_aliquota_credito, inv_line.icms_aliquota_credito)
                 self.assertEqual(line.tem_difal, inv_line.tem_difal)
+                self.assertEqual(line.icms_csosn_simples, inv_line.icms_csosn_simples)
+                self.assertEqual(line.icms_origem, inv_line.icms_origem)
+                self.assertEqual(line.incluir_ipi_base, inv_line.incluir_ipi_base)
+                self.assertEqual(line.icms_rule_id, inv_line.icms_rule_id)
                 self.assertEqual(line.ipi_cst, inv_line.ipi_cst)
                 self.assertEqual(line.ipi_reducao_bc, inv_line.ipi_reducao_bc)
+                self.assertEqual(line.ipi_rule_id, inv_line.ipi_rule_id)
                 self.assertEqual(line.pis_cst, inv_line.pis_cst)
+                self.assertEqual(line.pis_tipo, inv_line.pis_tipo)
+                self.assertEqual(line.pis_rule_id, inv_line.pis_rule_id)
                 self.assertEqual(line.cofins_cst, inv_line.cofins_cst)
+                self.assertEqual(line.cofins_tipo, inv_line.cofins_tipo)
+                self.assertEqual(line.cofins_rule_id, inv_line.cofins_rule_id)
+                self.assertEqual(line.issqn_tipo, inv_line.issqn_tipo)
+                self.assertEqual(line.ii_valor_despesas, inv_line.ii_valor_despesas)
+                self.assertEqual(line.ii_valor_iof, inv_line.ii_valor_iof)
+                self.assertEqual(line.ii_rule_id, inv_line.ii_rule_id)
+                self.assertEqual(line.csll_rule_id, inv_line.csll_rule_id)
+                self.assertEqual(line.irrf_rule_id, inv_line.irrf_rule_id)
+                self.assertEqual(line.inss_rule_id, inv_line.inss_rule_id)
 
     def test_generate_parcel_entry(self):
 
@@ -349,3 +378,117 @@ class TestBrSaleOrder(TransactionCase):
                                  parcel.parceling_value)
                 self.assertEqual(parcel_dict['amount_days'],
                                  parcel.amount_days)
+
+    def test_invoice_pis_cofins_taxes(self):
+
+        for order in self.sales_order:
+
+            first_item = order.order_line[0]
+
+            # PIS
+            first_item.tax_pis_id = self.pis_500
+            first_item._onchange_tax_pis_id()
+            self.assertEqual(first_item.price_total, 150.0)
+            self.assertEqual(first_item.pis_base_calculo, 150.0)
+            self.assertEqual(first_item.pis_valor, 7.5)
+            self.assertEqual(first_item.pis_aliquota, 5.0)
+
+            # COFINS
+            first_item.tax_cofins_id = self.cofins_1500
+            first_item._onchange_tax_cofins_id()
+            self.assertEqual(first_item.price_total, 150.0)
+            self.assertEqual(first_item.cofins_base_calculo, 150.0)
+            self.assertEqual(first_item.cofins_valor, 22.5)
+            self.assertEqual(first_item.cofins_aliquota, 15.0)
+
+            for item in order.order_line:
+                item.tax_pis_id = self.pis_500
+                item._onchange_tax_pis_id()
+                item._br_sale_onchange_product_id()
+                self.assertEqual(item.pis_base_calculo, item.price_total)
+                self.assertEqual(item.pis_aliquota, 5.0)
+                self.assertEqual(item.pis_valor, item.price_total * 0.05)
+
+                item.tax_cofins_id = self.cofins_1500
+                item._onchange_tax_cofins_id()
+                item._br_sale_onchange_product_id()
+                self.assertEqual(item.cofins_base_calculo, item.price_total)
+                self.assertEqual(item.cofins_aliquota, 15.0)
+                self.assertEqual(item.cofins_valor, item.price_total * 0.15)
+
+                self.assertEqual(len(item.tax_id), 2)
+
+    def test_invoice_issqn_and_ii_taxes(self):
+
+        for order in self.sales_order:
+            prod_item = order.order_line[0]
+            serv_item = order.order_line[1]
+
+            # II
+            prod_item.tax_ii_id = self.ii_6000
+            prod_item._onchange_tax_ii_id()
+            self.assertEqual(prod_item.price_total, 150.0)
+            self.assertEqual(prod_item.ii_base_calculo, 150.0)
+            self.assertEqual(prod_item.ii_valor, 90.0)
+            self.assertEqual(prod_item.ii_aliquota, 60.0)
+
+            # ISSQN
+            serv_item.tax_issqn_id = self.issqn_500
+            serv_item._onchange_tax_issqn_id()
+            self.assertEqual(serv_item.price_total, 500.0)
+            self.assertEqual(serv_item.issqn_base_calculo, 500.0)
+            self.assertEqual(serv_item.issqn_valor, 25.0)
+            self.assertEqual(serv_item.issqn_aliquota, 5.0)
+
+    def test_invoice_icms_normal_tax(self):
+
+        for order in self.sales_order:
+
+            first_item = order.order_line[0]
+
+            # ICMS
+            first_item.tax_icms_id = self.icms_1700
+            first_item._onchange_tax_icms_id()
+            self.assertEqual(first_item.price_total, 150.0)
+            self.assertEqual(first_item.icms_base_calculo, 150.0)
+            self.assertEqual(first_item.icms_valor, 25.5)
+            self.assertEqual(first_item.icms_aliquota, 17.0)
+
+            for item in order.order_line:
+                item.tax_icms_id = self.icms_1700
+                item._onchange_tax_icms_id()
+                item._br_sale_onchange_product_id()
+                self.assertEqual(item.icms_base_calculo, item.price_total)
+                self.assertEqual(
+                    item.icms_valor, round(item.price_total * 0.17, 2))
+                self.assertEqual(item.icms_aliquota, 17.0)
+
+                self.assertEqual(len(item.tax_id), 1)
+
+    def test_invoice_icms_reducao_base_tax(self):
+
+        for order in self.sales_order:
+
+            first_item = order.order_line[0]
+
+            # ICMS com Redução de base
+            first_item.tax_icms_id = self.icms_1700
+            first_item.icms_aliquota_reducao_base = 10.0
+            first_item._onchange_tax_icms_id()
+            self.assertEqual(first_item.price_total, 150.0)
+            self.assertEqual(first_item.icms_base_calculo, 135.0)
+            self.assertEqual(first_item.icms_valor, 22.95)
+            self.assertEqual(first_item.icms_aliquota, 17.0)
+
+            for item in order.order_line:
+                item.tax_icms_id = self.icms_1700
+                item.icms_aliquota_reducao_base = 10.0
+                item._onchange_tax_icms_id()
+                item._br_sale_onchange_product_id()
+                self.assertEqual(
+                    item.icms_base_calculo, round(item.price_total * 0.9, 2))
+                self.assertEqual(
+                    item.icms_valor, round(item.price_total * 0.9 * 0.17, 2))
+                self.assertEqual(item.icms_aliquota, 17.0)
+
+                self.assertEqual(len(item.tax_id), 1)
