412e07c8b40c084eb8270f0913f54ceac3b004cb
Michell Stuttgart <michellstut@gmail.com>
2018-07-12 15:35:45 -0300

    [NEW] Adiciona novo modulo 'br_calendar'

---
+++ br_calendar/README.md
@@ -0,0 +1,8 @@
+## BrCalendar
+
+Este modulo agrupa ajustes da localização para o módulo 'calendar' e ajustes
+mais gerais na view do mesmo modulo
+
+### Contributors
+
+* Michell Stuttgart <michellstut@gmail.com>,
---
+++ br_calendar/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Br Calendar',
+    'version': '11.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': """Modulo base da localização para o calendario""",
+    'contributors': [
+        'Michell Stuttgart <michellstut@gmail.com>',
+    ],
+    'depends': [
+        'calendar',
+    ],
+    'data': [
+        'views/calendar_menu.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
